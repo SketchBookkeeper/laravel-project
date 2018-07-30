3ad47d03a581e52a73a96526cb69ff3de40ab058
Paul Allen <p.allen.art@gmail.com>
2018-07-30 08:27:13 -0600

    Create comments model.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,4 +4,22 @@
     <h1>{{ $post->title }}</h1>
 
     {{ $post->body }}
+
+    <hr>
+
+    <div class="comments">
+        <ul class="list-group">
+            @foreach ($post->comments as $comment)
+                <li class="list-group-item">
+                    <article>
+                        <strong>
+                            {{ $comment->created_at->diffForHumans() }} :
+                        </strong>
+
+                        {{ $comment->body }}
+                    </article>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 @endsection
