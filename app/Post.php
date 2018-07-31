f258c860c579ad4741d38d03eec4610fcc10ee8a
Paul Allen <p.allen.art@gmail.com>
2018-07-30 23:24:32 -0600

    Add comments.

--- app/Post.php
+++ app/Post.php
@@ -6,8 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $guarded = [];
+
     public function comments()
     {
         return $this->hasMany(Comment::class);
+    }
+
+    public function addComment($body)
+    {
+        $this->comments()->create(['body' => $body]);
+        // Or we could use compact() to combine 'body' to the $body
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,13 +1,14 @@
 @extends ('layout')
 
 @section ('content')
+    <?php //dd($post); ?>
     <h1>{{ $post->title }}</h1>
 
     {{ $post->body }}
 
     <hr>
 
-    <div class="comments">
+    <div class="comments mb-3">
         <ul class="list-group">
             @foreach ($post->comments as $comment)
                 <li class="list-group-item">
@@ -22,4 +23,22 @@
             @endforeach
         </ul>
     </div>
+
+    <div class="card mb-3">
+        <div class="card-body">
+            <form method="POST" action="/posts/{{ $post->id }}/comments">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <textarea class="form-control" name="body" placeholder="Your Comment here" ></textarea>
+                </div>
+
+                <div class="form-group">
+                    <button class="btn btn-outline-primary" type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    @include ('partials.errors')
+
 @endsection
