e472738ea3ebb3937ed8cfff778480f6ef69c2e4
Paul Allen <p.allen.art@gmail.com>
2018-08-01 22:59:12 -0600

    View Composer.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -17,12 +17,6 @@ class PostsController extends Controller
             ->filter(request(['month', 'year']))
             ->get();
 
-        $archives = \App\Post::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
-            ->groupBy('year', 'month')
-            ->orderByRaw('min(created_at)')
-            ->get()
-            ->toArray();
-
         return view('posts.index', compact('posts', 'archives'));
     }
 
