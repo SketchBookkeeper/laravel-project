5e0c6461192067167fa5eb2cd3f90a75879a2ecb
Paul Allen <p.allen.art@gmail.com>
2018-08-04 23:46:18 -0600

    testing 101.

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Unit;
 
+use App\Post;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseTransactions;
     /**
      * A basic test example.
      *
@@ -14,6 +17,30 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        // Given I have two records in the database that are posts
+        // and each one is poste a month apart
+        $first = factory(Post::class)->create();
+        $second = factory(Post::class)->create([
+            'created_at' => \Carbon\Carbon::now()->subMonth()
+        ]);
+
+        // When I fetch archives
+        $posts = Post::archives();
+
+        // Run Test
+        $this->assertCount(2, $posts);
+
+        $this->assertEquals([
+            [
+                'year' => $second->created_at->format('Y'),
+                'month' => $second->created_at->format('F'),
+                'published' => 1
+            ],
+            [
+                'year' => $first->created_at->format('Y'),
+                'month' => $first->created_at->format('F'),
+                'published' => 1
+            ],
+        ], $posts);
     }
 }
